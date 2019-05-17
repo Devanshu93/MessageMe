c5c409a6cba5c75775ceeaf0004afedacbd5aced
Devanshu Row <devanshu.row1993@gmail.com>
2019-05-17 16:08:43 -0400

    Created models for user and message

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  validates :username, presence: true, 
+            uniqueness: { case_sensitive: false }, 
+            length: { minimum: 5, maximum: 20 }
+
+  has_secure_password
+  has_many :messages
+end
