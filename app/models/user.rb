79d7c271d77ba6954dfa6f8e9aca5cd2a97f2661
Devanshu Row <devanshu.row1993@gmail.com>
2019-05-24 19:08:08 -0400

    Updated views, controllers and routes to work perfectly

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
             uniqueness: { case_sensitive: false }, 
             length: { minimum: 5, maximum: 20 }
 
-  has_secure_password
   has_many :messages
+  has_secure_password
 end
